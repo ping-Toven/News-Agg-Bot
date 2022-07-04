b5a0dea1238fb8836483488083ea07dcd64a6f1d
ping-Toven <tomasoliva@outlook.com>
2022-07-03 21:28:23 -0400

    add checks, improved err handle, add >1 feed

--- catvenbot.py
+++ catvenbot.py
@@ -5,6 +5,7 @@ import os
 import sqlite3
 from sqlite3 import Error
 from dotenv import load_dotenv
+from setuptools import setup
 
 load_dotenv()
 logger = logging.getLogger('discord')
@@ -30,31 +31,6 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-#Function to check if guild has been set up in DB
-def setup_check(gid):
-    """ Check for the guild_id in the DB
-    :param gid: command's ctx guild id
-    :return bool:
-    """
-    cursor = RSS_conn.cursor()
-    cursor.execute("SELECT guild_id FROM RSS_Feeds WHERE guild_id = (?)", [gid])
-    if cursor.fetchone() is None:
-        return False
-    else:
-        return True
-
-#Function to add an RSS Feed url to the guild's DB
-def db_add_feed(feed_url, channel_id):
-    """ add a feed URL from slash command to the db
-    :param conn: Connection object
-    :param feed_url: RSS feed url submitted by user in command "add"
-    :param channel_id: Channel ID 
-    :return:
-    """
-    cursor = RSS_conn.cursor()
-    cursor.execute("INSERT INTO RSS_Feeds (feed_url) VALUES(?) WHERE channel_id=:cid", [feed_url], {"cid":channel_id})
-    RSS_conn.commit()
-
 #Function to setup a guild with a specific channel for RSS Feed posts
 def db_setup_guild_channel(guild_id, channel_id):
     """ add a guild_id:channel_id row in db
@@ -65,15 +41,78 @@ def db_setup_guild_channel(guild_id, channel_id):
     try:
         cursor = RSS_conn.cursor()
         #if false, insert new row
-        if setup_check(guild_id):
-                cursor.execute("INSERT INTO RSS_Feeds (guild_id, channel_id) VALUES (?,?)", [guild_id, channel_id])
-                RSS_conn.commit()
+        if not setup_check(guild_id):
+            cursor.execute("INSERT INTO RSS_Feeds (guild_id, channel_id) VALUES (?,?)", [guild_id, channel_id])
+            RSS_conn.commit()
         #else update existing
         else:
             cursor.execute("UPDATE RSS_Feeds SET channel_id=:cid WHERE guild_id=:gid", {"cid":channel_id, "gid":guild_id})
             RSS_conn.commit()
     except sqlite3.Error as error:
         print("Failed to setup guild's channel. Error: ", error)
+
+def get_feed_channel(guild_id):
+    """ fetch feed channel from guild
+    :param guild_id: Guild ID
+    :return str: Error or feed channel id
+    """
+    cursor = RSS_conn.cursor()
+    if not setup_check(guild_id):
+        return 
+    else:
+        cursor.execute("SELECT channel_id FROM RSS_Feeds WHERE guild_id=?",[guild_id])
+        feed_channel_id = str(cursor.fetchone()[0])
+        return feed_channel_id
+
+#Function to check if guild has been set up in DB
+def setup_check(gid):
+    """ Check for the guild_id in the DB
+    :param gid: command's ctx guild id
+    :return bool:
+    """
+    cursor = RSS_conn.cursor()
+    cursor.execute("SELECT guild_id FROM RSS_Feeds WHERE guild_id = (?)", [gid])
+    #SQL statement will return guild_id as provided by command if the server is set up. Otherwise returns None
+    if cursor.fetchone() is None:
+        return False
+    else:
+        return True
+
+#Function to check if Feed URL has been added to guild specific DB
+def feed_check(feed_url):
+    """ Check for the RSS Feed's URL in the Guild's DB
+    :param feed_url: command's feed URL
+    :return bool:
+    """
+    cursor = RSS_conn.cursor()
+    cursor.execute("SELECT feed_url FROM RSS_Feeds WHERE feed_url = (?)", [feed_url])
+    #SQL statement will return feed_url as provided by command if this feed has already been added. Otherwise returns None
+    if cursor.fetchone() is None:
+        return False
+    else:
+        return True
+
+#Function to add an RSS Feed url to the guild's DB
+def db_add_feed(RSS_url, guild_id):
+    """ add a feed URL from slash command to the db
+    :param conn: Connection object
+    :param feed_url: RSS feed url submitted by user in command "add"
+    :param guild_id: Guild ID
+    :param channel_id: Channel ID
+    :return str: one of the 3 statuses
+    """
+    existing_feed = "Feed already added"
+    successful = "RSS Feed added successfully"
+    feed_channel_id = get_feed_channel(guild_id)
+    cursor = RSS_conn.cursor()
+    if feed_check(RSS_url):
+        return existing_feed
+    else:
+        cursor.execute("INSERT INTO RSS_Feeds (guild_id, channel_id, feed_url) VALUES (?,?,?)", [guild_id, feed_channel_id, RSS_url])
+        RSS_conn.commit()
+        return successful
+
+
 
 #Write to terminal on login
 @testBot.event
@@ -92,22 +131,20 @@ async def hello(ctx):
     await ctx.respond("Hey!")
 
 #Add RSS feed command
-@testBot.slash_command(name = "add", description = "Add an RSS feed to the bot's watchlist.")
-async def add(ctx, feed_url: str): 
-    cursor = RSS_conn.cursor()
+@testBot.slash_command(name = "add_rss", description = "Add an RSS feed to the bot's watchlist.")
+async def add_rss(ctx, feed_url: str): 
     guild_id = str(ctx.guild.id)
-    try:
-        cursor.execute("SELECT guild_id FROM RSS_Feeds WHERE guild_id = (?)", [guild_id])
-        if cursor.fetchone() is None:
-            await ctx.respond("Please make sure to run the /setup command")
-        else:
-            cursor
-    except:
-        ...
+    #if false prompt user to run /setup command
+    if not setup_check(guild_id):
+        await ctx.respond("Please make sure to run the /setup command")
+        return
+    channel = ctx.channel
+    status = db_add_feed(feed_url, guild_id)
+    await ctx.respond(status + f" in {channel.mention}")
 
 #Remove RSS feed command
-@testBot.slash_command(name = "remove", description = "Remove an RSS feed from the bot's watchlist.")
-async def add(ctx, feed_url: str): 
+@testBot.slash_command(name = "remove_rss", description = "Remove an RSS feed from the bot's watchlist.")
+async def remove_rss(ctx, feed_url: str): 
     ...
 
 #Setup channel for RSS feed command
@@ -126,7 +163,7 @@ async def info(ctx):
     guild = ctx.guild
     try:
         #if false prompt user to run /setup command
-        if setup_check(guild_id):
+        if not setup_check(guild_id):
             await ctx.respond("Please make sure to run the /setup command")
         #else list information regarding channel config
         else:
