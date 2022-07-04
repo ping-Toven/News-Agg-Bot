2dec9fde82ca167eceb126458009202009df6a36
ping-Toven <tomasoliva@outlook.com>
2022-07-03 20:10:19 -0400

    renaming files

--- catvenbot.py
+++ catvenbot.py
@@ -16,7 +16,6 @@ RSS_database = "RSS_Feeds.db"
 testBot = discord.Bot()
 RSS_conn = sqlite3.connect(RSS_database)
 
-
 #Function to create the tables & set cursor
 def create_table(conn, create_table_sql):
     """ create a table from the create_table_sql statement
@@ -31,6 +30,20 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
+#Function to check if guild has been set up in DB
+def setup_check(gid):
+    """ Check for the guild_id in the DB
+    :param gid: command's ctx guild id
+    :return bool:
+    """
+    cursor = RSS_conn.cursor()
+    cursor.execute("SELECT guild_id FROM RSS_Feeds WHERE guild_id = (?)", [gid])
+    if cursor.fetchone() is None:
+        return False
+    else:
+        return True
+
+#Function to add an RSS Feed url to the guild's DB
 def db_add_feed(feed_url, channel_id):
     """ add a feed URL from slash command to the db
     :param conn: Connection object
@@ -42,6 +55,7 @@ def db_add_feed(feed_url, channel_id):
     cursor.execute("INSERT INTO RSS_Feeds (feed_url) VALUES(?) WHERE channel_id=:cid", [feed_url], {"cid":channel_id})
     RSS_conn.commit()
 
+#Function to setup a guild with a specific channel for RSS Feed posts
 def db_setup_guild_channel(guild_id, channel_id):
     """ add a guild_id:channel_id row in db
     :param guild_id: Guild ID
@@ -50,10 +64,11 @@ def db_setup_guild_channel(guild_id, channel_id):
     """
     try:
         cursor = RSS_conn.cursor()
-        cursor.execute("SELECT guild_id FROM RSS_Feeds WHERE guild_id = (?)", [guild_id])
-        if cursor.fetchone() is None:
+        #if false, insert new row
+        if setup_check(guild_id):
                 cursor.execute("INSERT INTO RSS_Feeds (guild_id, channel_id) VALUES (?,?)", [guild_id, channel_id])
                 RSS_conn.commit()
+        #else update existing
         else:
             cursor.execute("UPDATE RSS_Feeds SET channel_id=:cid WHERE guild_id=:gid", {"cid":channel_id, "gid":guild_id})
             RSS_conn.commit()
@@ -79,7 +94,16 @@ async def hello(ctx):
 #Add RSS feed command
 @testBot.slash_command(name = "add", description = "Add an RSS feed to the bot's watchlist.")
 async def add(ctx, feed_url: str): 
-    ...
+    cursor = RSS_conn.cursor()
+    guild_id = str(ctx.guild.id)
+    try:
+        cursor.execute("SELECT guild_id FROM RSS_Feeds WHERE guild_id = (?)", [guild_id])
+        if cursor.fetchone() is None:
+            await ctx.respond("Please make sure to run the /setup command")
+        else:
+            cursor
+    except:
+        ...
 
 #Remove RSS feed command
 @testBot.slash_command(name = "remove", description = "Remove an RSS feed from the bot's watchlist.")
@@ -101,15 +125,15 @@ async def info(ctx):
     guild_id = str(ctx.guild.id)
     guild = ctx.guild
     try:
-        cursor.execute("SELECT guild_id FROM RSS_Feeds WHERE guild_id = (?)", [guild_id])
-        if cursor.fetchone() is None:
+        #if false prompt user to run /setup command
+        if setup_check(guild_id):
             await ctx.respond("Please make sure to run the /setup command")
+        #else list information regarding channel config
         else:
             cursor.execute("SELECT channel_id FROM RSS_Feeds WHERE guild_id=:gid", {"gid":guild_id})
             feed_channel_id = str(cursor.fetchone()[0])
             feed_channel_mention = "<#" + feed_channel_id + ">"
             await ctx.respond(f"{guild}'s RSS Feed channel is set to {feed_channel_mention}")
-
     except sqlite3.Error as error:
         print("info command error: " , error)
         await ctx.respond("Please make sure to run the /setup command")
