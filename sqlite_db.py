22b4ddae23077f2b5bbefd926a327ba63059ff6d
ping-Toven <tomasoliva@outlook.com>
2022-07-21 20:56:06 -0400

    Restructured db helper classes

--- sqlite_db.py
+++ sqlite_db.py
@@ -22,20 +22,19 @@ class SQLITE_DB:
         :param db_name: name of database to connect to
         :return sqlite3.Connection: returns connection object
         """
-            conn_feeds = sqlite3.connect("Feeds.db")
-            return conn_feeds
+        conn_feeds = sqlite3.connect("Feeds.db")
+        return conn_feeds
 
     # Function to check if guild has been set up in DB
-    def setup_check(self, conn: sqlite3.Connection, gid: str):
+    def setup_check(self, conn: sqlite3.Connection, gid: str, table: str):
         """ Check for the guild_id in the DB
+        :param table: table to check in db
         :param conn: sqlite3 connection obj
         :param gid: command's ctx guild id
         :return bool: True = guild has been set up in this DB, False = not set up
         """
         cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        table_name, = cursor.fetchone()
-        cursor.execute("SELECT guild_id FROM {} WHERE guild_id = (?)".format(table_name), [gid])
+        cursor.execute("SELECT guild FROM {} WHERE guild = (?)".format(table), [gid])
         # SQL statement will return guild_id as provided by command if the DBs are set up. Otherwise, returns None
         if cursor.fetchone() is None:
             return False
@@ -43,8 +42,9 @@ class SQLITE_DB:
             return True
 
     # Function to set up a guild with a specific channel id for a feed (RSS & Twitter separate dbs / channels)
-    def setup_guild_channel(self, conn: sqlite3.Connection, guild_id: str, channel_id: str):
+    def setup_guild_channel(self, conn: sqlite3.Connection, guild_id: str, channel_id: str, table: str):
         """ add a guild_id:channel_id row in db
+        :param table: table within which to set up this guild
         :param conn: Connection object
         :param guild_id: Guild ID str
         :param channel_id: Channel ID str
@@ -52,49 +52,46 @@ class SQLITE_DB:
         """
         try:
             cursor = conn.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-            table_name, = cursor.fetchone()
             # if setup has not happened, insert new row
             if not self.setup_check(self, conn, guild_id):
-                cursor.execute("INSERT INTO {} (guild_id, channel_id) VALUES (?,?)".format(table_name),
+                cursor.execute("INSERT INTO {} (guild, channel) VALUES (?,?)".format(table),
                                [guild_id, channel_id])
                 conn.commit()
             # else update existing
             else:
-                cursor.execute("UPDATE {} SET channel_id = (?) WHERE guild_id = (?)".format(table_name),
+                cursor.execute("UPDATE {} SET channel = (?) WHERE guild = (?)".format(table),
                                [channel_id, guild_id])
                 conn.commit()
         except sqlite3.Error as error:
             print("Failed to setup guild's channel. Error: ", error)
 
-    def get_posting_channel(self, conn: sqlite3.Connection, guild_id: str):
+    def get_posting_channel(self, conn: sqlite3.Connection, guild_id: str, table: str):
         """ fetch posting channel from guild
+        :param table: table within which to grab channel from
         :param conn: db connection obj
         :param guild_id: Guild ID str
         :return str: Error or feed channel id
         """
         cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        table_name, = cursor.fetchone()
         # if setup has not happened, abort
         if not self.setup_check(self, conn, guild_id):
             return
         else:
-            cursor.execute("SELECT channel_id FROM {} WHERE guild_id=(?)".format(table_name), [guild_id])
+            cursor.execute("SELECT channel FROM {} WHERE guild = (?)".format(table), [guild_id])
             feed_channel_id, = cursor.fetchone()
             return feed_channel_id
 
-    # Function to check if feed_source has been added to guild specific DB
-    def feed_check(conn: sqlite3.Connection, feed_key: str, gid: str):
+    # Function to check if a specified feed source has been added to a specified guild
+    def feed_check(self, conn: sqlite3.Connection, feed_key: str, gid: str, table: str):
         """ Check for a feed value in the Guild's DB
-        :param search_key: command's feed input to check against
+        :param table: table within which to check feed_key for
+        :param conn: sqlite3 connection obj
+        :param feed_key: command's feed input to check against
         :param gid: Guild ID in which to look for search_key
         :return bool:
         """
         cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        table_name, = cursor.fetchone()
-        cursor.execute("SELECT feed_source FROM {} WHERE guild_id = (?)".format(table_name), [gid])
+        cursor.execute("SELECT source FROM {} WHERE guild = (?)".format(table), [gid])
         guild_feeds = cursor.fetchall()
         for feed, in guild_feeds:
             if feed == feed_key:
@@ -102,8 +99,9 @@ class SQLITE_DB:
         return False
 
     # Function to add a feed source to the guild's DB
-    def add_feed(self, conn: sqlite3.Connection, feed_source: str, guild_id: str):
+    def add_feed(self, conn: sqlite3.Connection, feed_source: str, guild_id: str, table:str):
         """ add a feed source from slash command to the db
+        :param table: table within which to add the feed_source to
         :param conn: sqlite3 connection object
         :param feed_source: feed source submitted by user in command "add"
         :param guild_id: Guild ID
@@ -111,21 +109,20 @@ class SQLITE_DB:
         """
         existing_feed = "Feed already added"
         successful = "Feed added successfully"
-        feed_channel_id = self.get_posting_channel(self, conn, guild_id)
+        feed_channel_id = self.get_posting_channel(self, conn=conn, guild_id=guild_id, table=table)
         cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        table_name, = cursor.fetchone()
-        if self.feed_check(conn, feed_source, guild_id):
+        if self.feed_check(self, conn, feed_source, guild_id, table):
             return existing_feed
         else:
-            cursor.execute("INSERT INTO {} (guild_id, channel_id, feed_source) VALUES (?,?,?)".format(table_name),
+            cursor.execute("INSERT INTO {} (guild, channel, source) VALUES (?,?,?)".format(table),
                            [guild_id, feed_channel_id, feed_source])
             conn.commit()
             return successful
 
     # Function to remove an existing RSS Feed url from guild's DB
-    def remove_feed(self, conn: sqlite3.Connection, feed_source: str, guild_id: str):
+    def remove_feed(self, conn: sqlite3.Connection, feed_source: str, guild_id: str, table: str):
         """ remove an existing feed URL from guild specific DB entries
+        :param table: table within which to remove feed_source
         :param conn: sqlite3 connection object
         :param feed_source: feed submitted by user in command
         :param guild_id: Guild ID
@@ -134,25 +131,21 @@ class SQLITE_DB:
         unknown_feed = "Feed does not exist"
         successful = "Feed removed successfully"
         cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        table_name, = cursor.fetchone()
-        if not self.feed_check(conn, feed_source, guild_id):
+        if not self.feed_check(self, conn, feed_source, guild_id, table):
             return unknown_feed
         else:
-            cursor.execute("DELETE FROM {} WHERE feed_source = (?) AND guild_id = (?)".format(table_name),
+            cursor.execute("DELETE FROM {} WHERE source = (?) AND guild = (?)".format(table),
                            [feed_source, guild_id])
             conn.commit()
             return successful
 
-    def list_feeds(self, conn: sqlite3.Connection):
-        """ Fetch all feeds from a single DB
+    def list_feeds(self, conn: sqlite3.Connection, table: str):
+        """ Fetch all guild:feeds in a list from a single table
+        :param table: table name from which to pull
         :param conn: sqlite3 connection object
-        :param guild_id: Guild ID
-        :return feed_list: returns a list of all the feeds in tuple syntax (a,b,c)
+        :return feed_list: returns a list of all the feeds
         """
         cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        table_name, = cursor.fetchone()
-        cursor.execute("SELECT guild_id, feed_source FROM {}".format(table_name))
+        cursor.execute("SELECT guild, source FROM {}".format(table))
         feed_list = cursor.fetchall()
         return feed_list
